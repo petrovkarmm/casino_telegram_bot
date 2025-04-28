7f014eb3c82087b76e6a1cc892821b6cd02abdfd
Petrov Leonid <petrovleonid1999@mail.ru>
2025-04-28 20:03:31 +0300

    error unknown intent handler (deleted old windows)

--- bot.py
+++ bot.py
@@ -1,7 +1,9 @@
 import asyncio
 
 from aiogram import Bot, Dispatcher
-from aiogram_dialog import setup_dialogs
+from aiogram.types import ErrorEvent
+from aiogram_dialog import setup_dialogs, DialogManager
+from aiogram_dialog.api.exceptions import UnknownIntent, OutdatedIntent
 
 from settings import bot_token
 from start_menu.casino_dialog.casino_dialog_router import casino_dialog_router
@@ -13,6 +15,27 @@ async def bot_start():
     bot = Bot(token=bot_token)
     dp = Dispatcher()
     setup_dialogs(dp)
+
+    async def error_unknown_intent_handler(
+            event: ErrorEvent, dialog_manager: DialogManager
+    ):
+        if isinstance(event.exception, UnknownIntent) or isinstance(event.exception, OutdatedIntent):
+            try:
+                event_message_id = event.update.callback_query.message.message_id
+                event_chat_id = event.update.callback_query.message.chat.id
+                await bot.delete_message(
+                    chat_id=event_chat_id, message_id=event_message_id
+                )
+            except AttributeError:
+                print(f'Отбилась в закрытый диалог.')
+            except Exception as exception:
+                print(exception)
+
+        else:
+            return print(f"{event}")
+
+    # error handler
+    dp.errors.register(error_unknown_intent_handler)
 
     # handlers routers
     dp.include_router(start_menu_router)
