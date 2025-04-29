3f583c63f9eb622fb9a5849218650d9d643f8d22
Petrov Leonid <petrovleonid1999@mail.ru>
2025-04-29 18:02:35 +0300

    spin roulette test

--- start_menu/casino_dialog/casino_on_click_functions.py
+++ start_menu/casino_dialog/casino_on_click_functions.py
@@ -1,10 +1,12 @@
+import asyncio
+import random
 from pprint import pprint
 
 from aiogram.types import CallbackQuery
-from aiogram_dialog import DialogManager
+from aiogram_dialog import DialogManager, BaseDialogManager, ShowMode
 from aiogram_dialog.widgets.kbd import Button, ManagedCounter
-
-from start_menu.casino_dialog.casino_data import BET_TYPES
+from aiogram.exceptions import TelegramRetryAfter
+from start_menu.casino_dialog.casino_data import BET_TYPES, wheel
 from start_menu.casino_dialog.casino_dialog_states import CasinoDialog
 from start_menu.casino_dialog.utils import parse_bet_slug
 
@@ -17,6 +19,52 @@ async def close_dialog(
     except Exception as e:
         print(e)
         pass
+
+
+async def spin_roulette(callback: CallbackQuery, button: Button, dialog_manager: DialogManager):
+    dialog_manager.dialog_data["roulette_spin"] = "⏳ Крутим..."
+    await dialog_manager.switch_to(CasinoDialog.roulette_spin)
+
+    # Получаем фонового менеджера сразу после переключения состояния
+    await check_roulette_spin(dialog_manager)
+
+
+# Фоновая функция прокрутки
+async def check_roulette_spin(bg_manager: DialogManager):
+    spins = random.randint(25, 35)
+    delay = 0.1
+
+    for i in range(spins):
+        index = i % len(wheel)
+        number, color = wheel[index]
+
+        view = []
+        for j, (n, c) in enumerate(wheel[index:index + 7]):
+            if j == 0:
+                view.append(f'🔘{c}{n}')
+            else:
+                view.append(f'{c}{n}')
+        display = ' | '.join(view)
+
+        # Только каждую третью итерацию обновляем сообщение
+        if i % 3 == 0:
+            try:
+                await bg_manager.update({"roulette_spin": display})
+            except Exception as e:
+                print(f"Ошибка обновления сообщения: {e}")
+
+        await asyncio.sleep(delay)
+        delay *= 1.07
+
+    # Результат
+    result_number, result_color = wheel[index]
+    color_name = {"🔴": "красное", "⚫": "черное", "🟩": "зеленое"}[result_color]
+    result_text = f'🎉 Выпало: {result_color}{result_number} — {color_name.upper()}!'
+
+    try:
+        await bg_manager.update({"roulette_spin": result_text})
+    except Exception as e:
+        print(f"Ошибка финального обновления: {e}")
 
 
 async def choose_set(
