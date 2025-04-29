324628154f36ea46e758f35374861da50d64f2a3
Petrov Leonid <petrovleonid1999@mail.ru>
2025-04-29 12:56:17 +0300

    bet getter

--- start_menu/casino_dialog/casino_getters.py
+++ start_menu/casino_dialog/casino_getters.py
@@ -8,20 +8,10 @@ async def first_window_start_data(dialog_manager: DialogManager, **_kwargs):
     return dialog_manager.start_data
 
 
-async def bet_getter(dialog_manager: ManagedCounter, **kwargs):
-    bet_counter: ManagedCounter = dialog_manager.find('roulette_bet_counter')
-    bet = bet_counter.get_value()
+async def balance_getter(dialog_manager: ManagedCounter, **kwargs):
     balance = dialog_manager.start_data['balance']
     coefficient = dialog_manager.dialog_data['coefficient']
 
-    potential_gain = bet * coefficient
-
-    if bet == balance:
-        return None
-    else:
-        return {
-            'balance': balance,
-            'bet': bet,
-            'potential_gain': potential_gain
-        }
-
+    return {
+        'balance': balance
+    }
